ab01eeab5619e8d064ff420225f5b2584c1fc92c
Trung Hải <trunghai@Trungs-MacBook-Air.local>
2021-11-19 15:32:44 +0700

    remove log

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import LoginScreen from './components/LoginScreen';
+import "./App.css";
+import LoginScreen from "./components/LoginScreen";
 
 function App() {
   return (
